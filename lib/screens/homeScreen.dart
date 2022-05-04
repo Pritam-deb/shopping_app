116852298e617da09a829a7334a2bd7851efb0fe
Pritam-debnath <pritamd@geekyants.com>
2022-05-04 16:19:35 +0530

    created a basic app for working on

---
+++ lib/screens/homeScreen.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [Icon(Icons.horizontal_split), Icon(Icons.search)],
+        ),
+      ),
+      body: Center(
+        child: Text('This is body'),
+      ),
+    );
+  }
+}
