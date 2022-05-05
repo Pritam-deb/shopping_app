39bb40c3a788bd292ae10e398d7be3dc64f24269
Pritam-debnath <pritamd@geekyants.com>
2022-05-05 18:20:09 +0530

    created the upper part of home page

--- lib/screens/homeScreen.dart
+++ lib/screens/homeScreen.dart
@@ -18,30 +18,123 @@ class _HomeScreenState extends State<HomeScreen> {
     NetworkRequests().fetchProducts(context);
   }
 
+  bool _pressAttention = false;
   @override
   Widget build(BuildContext context) {
     SizeConfig().init(context);
     return Scaffold(
-      appBar: AppBar(
-        title: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: const [
-            Icon(Icons.horizontal_split),
-            Icon(Icons.search),
-          ],
+      appBar: PreferredSize(
+        preferredSize: const Size.fromHeight(60),
+        child: AppBar(
+          backgroundColor: Colors.white,
+          elevation: 0,
+          title: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              const Icon(
+                Icons.horizontal_split,
+                color: Colors.black,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  const Icon(
+                    Icons.search,
+                    color: Colors.black,
+                  ),
+                  const Icon(
+                    Icons.shopping_bag_outlined,
+                    color: Colors.black,
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
-      body: Consumer<ProductProvider>(
-        builder: ((context, _provider, child) {
-          return _provider.productList.isNotEmpty
-              ? Text(_provider.productList[0].genre,
-                  style: TextStyle(
-                    fontSize: 20.toFont,
-                  ))
-              : const Text(
-                  "nothing yet :(",
-                );
-        }),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const Padding(
+            padding: EdgeInsets.fromLTRB(18, 0, 0, 20),
+            child: Text(
+              'Find your Style',
+              //textAlign: TextAlign.end,
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 20,
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                OutlinedButton(
+                  style: OutlinedButton.styleFrom(
+                    primary: _pressAttention ? Colors.white : Colors.black,
+                    backgroundColor:
+                        _pressAttention ? Colors.black : Colors.white,
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10)),
+                  ),
+                  onPressed: () =>
+                      setState(() => _pressAttention = !_pressAttention),
+                  child: const Text('All'),
+                ),
+                OutlinedButton(
+                  style: OutlinedButton.styleFrom(
+                    primary: _pressAttention ? Colors.white : Colors.black,
+                    backgroundColor:
+                        _pressAttention ? Colors.black : Colors.white,
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10)),
+                  ),
+                  onPressed: () =>
+                      setState(() => _pressAttention = !_pressAttention),
+                  child: const Text('Winter'),
+                ),
+                OutlinedButton(
+                  style: OutlinedButton.styleFrom(
+                    primary: _pressAttention ? Colors.white : Colors.black,
+                    backgroundColor:
+                        _pressAttention ? Colors.black : Colors.white,
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10)),
+                  ),
+                  onPressed: () =>
+                      setState(() => _pressAttention = !_pressAttention),
+                  child: const Text('Women'),
+                ),
+                OutlinedButton(
+                  style: OutlinedButton.styleFrom(
+                    primary: _pressAttention ? Colors.white : Colors.black,
+                    backgroundColor:
+                        _pressAttention ? Colors.black : Colors.white,
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10)),
+                  ),
+                  onPressed: () =>
+                      setState(() => _pressAttention = !_pressAttention),
+                  child: const Text('Eyewear'),
+                ),
+              ],
+            ),
+          ),
+          Consumer<ProductProvider>(
+            builder: ((context, _provider, child) {
+              return _provider.productList.isNotEmpty
+                  ? Text(_provider.productList[0].genre,
+                      style: TextStyle(
+                        fontSize: 20.toFont,
+                      ))
+                  : const Text(
+                      "nothing yet :(",
+                    );
+            }),
+          ),
+        ],
       ),
     );
   }
